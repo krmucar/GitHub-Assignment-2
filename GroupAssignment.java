6ec75d600c7361e8b04d15c9da3e128d9927275c
Artun Tuna Tac <149518718+cartoonytouch@users.noreply.github.com>
2026-01-28 16:47:14 +0300

    Added Min Max

--- GroupAssignment.java
+++ GroupAssignment.java
@@ -1,5 +1,25 @@
 public class GroupAssignment {
+    public static void getMin(int[] arr){
+        int minVal = Integer.Max_VALUE;
+        for(int i = 0; i < arr.length ; i++){
+            if(arr[i] < minVal){
+                minVal= arr[i];
+            }
+        }
+        System.out.println("Min value is: " + minVal);
+    }
+
+    public static void getMax(int[] arr){
+        int maxVal = 0;
+        for(int i = 0; i < arr.length ; i++){
+            if(arr[i] > maxVal){
+                maxVal= arr[i];
+            }
+        }
+        System.out.println("Max value is: " + maxVal);
+    }
     public static void main(String[] args) {
+        
         
     }
 }
