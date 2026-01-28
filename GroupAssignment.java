ef3d351b779581427653b7f3ccebb84ad2cd120d
Artun Tuna Tac <149518718+cartoonytouch@users.noreply.github.com>
2026-01-28 18:49:45 +0300

    Fixed array length issue
    Also fixed the case where numbers are all negatives

--- GroupAssignment.java
+++ GroupAssignment.java
@@ -2,23 +2,27 @@ import java.util.Scanner;
 
 public class GroupAssignment {
     public static void getMin(int[] arr){
-        int minVal = Integer.MAX_VALUE;
-        for(int i = 0; i < arr.length ; i++){
-            if(arr[i] < minVal){
-                minVal= arr[i];
+        if(arr.length > 0){
+            int minVal = arr[0];
+            for(int i = 1; i < arr.length ; i++){
+                if(arr[i] < minVal){
+                    minVal= arr[i];
+                }
             }
+            System.out.println("Min value is: " + minVal);
         }
-        System.out.println("Min value is: " + minVal);
     }
 
     public static void getMax(int[] arr){
-        int maxVal = 0;
-        for(int i = 0; i < arr.length ; i++){
+        if(arr.length > 0){
+            int maxVal = arr[0];
+        for(int i = 1; i < arr.length ; i++){
             if(arr[i] > maxVal){
                 maxVal= arr[i];
             }
         }
         System.out.println("Max value is: " + maxVal);
+        }
     }
         
     public static void findAvrgDiff(int[] arr){
