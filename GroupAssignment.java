585903409865de61bd0170e788dddb2635da096c
Artun Tuna Tac <149518718+cartoonytouch@users.noreply.github.com>
2026-01-28 16:34:13 +0300

    Added Max

--- GroupAssignment.java
+++ GroupAssignment.java
@@ -1,4 +1,15 @@
 public class GroupAssignment {
+    public static int getMax(int[] arr){
+        int maxVal = 0;
+        for(int i = 0; i < arr.length ; i++){
+            if(arr[i]> maxVal){
+                maxVal= arr[i];
+            }
+        }
+        return maxVal;
+    }
+
+    
     public static void main(String[] args) {
         
     }
