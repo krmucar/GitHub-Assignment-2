cbdcaeb03f641b576a7141638802d43f60f02088
Pixelated <196090898+Pxltd@users.noreply.github.com>
2026-01-28 16:56:29 +0300

    createArray() method written

--- GroupAssignment.java
+++ GroupAssignment.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class GroupAssignment {
     public static void main(String[] args) {
         
+    }
+
+    public static Array[] createArray()
+    {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter size: ");
+        int size = scan.nextInt();
+        int[] numbers = new int[size];
+        for (int i = 0; i<size; i++)
+        {
+            numbers[i] = (int)(Math.random()*100);
+        }
+        return numbers;
     }
 }
